4b2c3fe6ce95a45cebfdaa997e00070b63e4e625
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-12-03 19:30:38 +0530

    ..

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -96,7 +96,7 @@ def drawBoundingBox(saved_image ,x, y, w, h, cl, cf):
 
 
 def predict(model, url):
-    return model.predict(url, confidence=40, overlap=30).json()
+    return model.predict(url, confidence=40, overlap=30, agnostic_nms=True).json()
     #return model.predict(url, hosted=True).json()
 	
 	
